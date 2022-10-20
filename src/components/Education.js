8e14cdd2ee1b45004d15fb42d27c8323d625d6d4
Son Do <dominhson580@gmail.com>
2022-10-20 23:23:17 +0700

    Add input fields to Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 
 class Education extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="school">
+          <div className="field">
+            <label>School Name</label>
+            <input type="text" />
+          </div>
+          <div className="field">
+            <label>Title of Study</label>
+            <input type="text" />
+          </div>
+          <div className="field">
+            <label>Date of study</label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <button>Add school</button>
+      </div>
+    );
   }
 }
 
