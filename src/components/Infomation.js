0e146a8fe289bb5667d2fea26f1d49b006623ceb
Son Do <dominhson580@gmail.com>
2022-10-20 23:16:39 +0700

    Add input fields to Information

--- src/components/Infomation.js
+++ src/components/Infomation.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 
 class Information extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="field">
+          <label htmlFor="fullName">Full Name</label>
+          <input type="text" name="fullName" id="fullName" />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="phone">Phone</label>
+          <input type="number" name="phone" id="phone" />
+        </div>
+      </div>
+    );
   }
 }
 
