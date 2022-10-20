147a3c406ce2ceb25fe7c0a6c52ac72ef77e517b
Son Do <dominhson580@gmail.com>
2022-10-20 23:07:02 +0700

    Add heading and button to App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Education from "./components/Education";
 import Experience from "./components/Experience";
 
 class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     editMode: true,
+  //   };
+  // }
+
   render() {
     return (
       <div>
+        <h1>My CV</h1>
         <Information />
         <Education />
         <Experience />
+        <p>
+          <button>Submit</button>
+        </p>
       </div>
     );
   }
