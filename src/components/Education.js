114bb27253ca0298b9973c65371c75d45da8b543
Son Do <dominhson580@gmail.com>
2022-10-20 23:25:45 +0700

    Pass editMode as prop to School

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,24 +1,11 @@
 import React, { Component } from "react";
+import School from "./School";
 
 class Education extends Component {
   render() {
     return (
       <div>
-        <div className="school">
-          <div className="field">
-            <label>School Name</label>
-            <input type="text" />
-          </div>
-          <div className="field">
-            <label>Title of Study</label>
-            <input type="text" />
-          </div>
-          <div className="field">
-            <label>Date of study</label>
-            <input type="text" />
-          </div>
-        </div>
-
+        <School editMode={this.props.editMode} />
         <button>Add school</button>
       </div>
     );
