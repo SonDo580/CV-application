950a3c1b149b1327b430b62c758cffe7219f55af
Son Do <dominhson580@gmail.com>
2022-10-21 22:29:49 +0700

    Add class 'app' to the container

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <h1>My CV</h1>
         <hr />
         <Information editMode={this.state.editMode} />
