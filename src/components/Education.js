6fc48eb720e66a8849d08f1ef6bf2e8e0fe0bdb5
Son Do <dominhson580@gmail.com>
2022-10-21 21:44:56 +0700

    Add a school container

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import School from "./School";
 
 class Education extends Component {
+  // constructor() {
+  //   super();
+  // }
+
   render() {
     return (
       <div>
-        <School editMode={this.props.editMode} />
+        <div className="schools">
+          <School editMode={this.props.editMode} />
+        </div>
         <button>Add school</button>
       </div>
     );
