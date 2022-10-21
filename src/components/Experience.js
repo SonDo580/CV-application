b8dd0edc22917544a2021df5ed7480e7ee761ba6
Son Do <dominhson580@gmail.com>
2022-10-21 22:10:26 +0700

    Define state

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import Company from "./Company";
 
 class Experience extends Component {
+  constructor() {
+    super();
+    this.state = {
+      companies: [""],
+    };
+  }
+
   render() {
     return (
       <div>
