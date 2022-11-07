931267f9ab56d824b4e140c739971c122f5f4050
Son Do <dominhson580@gmail.com>
2022-11-07 16:50:19 +0700

    Let user enters 1 company
    Add more functionality later

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,32 +1,26 @@
 import React, { Component } from "react";
-import Company from "./Company";
 
 class Experience extends Component {
-  constructor() {
-    super();
-    this.state = {
-      companies: [""],
-    };
-  }
-
-  addCompany = () => {
-    this.setState((state) => ({
-      companies: state.companies.concat(""),
-    }));
-  };
-
   render() {
     return (
       <div className="container">
         <h2>Experience</h2>
-        <div className="companies">
-          {this.state.companies.map(() => {
-            return <Company editMode={this.props.editMode} />;
-          })}
+        <div className="field">
+          <label>Company Name</label>
+          <input type="text" />
         </div>
-        <p className="buttonContainer">
-          <button onClick={this.addCompany}>Add Company</button>
-        </p>
+        <div className="field">
+          <label>Position Title</label>
+          <input type="text" />
+        </div>
+        <div className="field">
+          <label>Main Tasks</label>
+          <input type="text" />
+        </div>
+        <div className="field">
+          <label>Date from and until</label>
+          <input type="text" />
+        </div>
       </div>
     );
   }
