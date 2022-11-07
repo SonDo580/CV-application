0064c4cceae526884ba4961ba9109cbf8ef20fc3
Son Do <dominhson580@gmail.com>
2022-11-07 16:48:02 +0700

    Let user enters 1 school
    I will add more functionality later

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,32 +1,24 @@
 import React, { Component } from "react";
-import School from "./School";
 
 class Education extends Component {
-  constructor() {
-    super();
-    this.state = {
-      schools: [""],
-    };
-  }
-
-  addSchool = () => {
-    this.setState((state) => ({
-      schools: state.schools.concat(""),
-    }));
-  };
-
   render() {
     return (
       <div className="container">
         <h2>Education</h2>
-        <div className="schools">
-          {this.state.schools.map(() => {
-            return <School editMode={this.props.editMode} />;
-          })}
+        <div className="school">
+          <div className="field">
+            <label>School Name</label>
+            <input type="text" />
+          </div>
+          <div className="field">
+            <label>Title of Study</label>
+            <input type="text" />
+          </div>
+          <div className="field">
+            <label>Date of study</label>
+            <input type="text" />
+          </div>
         </div>
-        <p className="buttonContainer">
-          <button onClick={this.addSchool}>Add School</button>
-        </p>
       </div>
     );
   }
