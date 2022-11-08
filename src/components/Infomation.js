80f08f1b6229b63e0481beeeaa6773841da6c2d4
Son Do <dominhson580@gmail.com>
2022-11-08 17:17:36 +0700

    Use conditional rendering

--- src/components/Infomation.js
+++ src/components/Infomation.js
@@ -30,68 +30,60 @@ class Information extends Component {
       <form onSubmit={this.handleSubmit}>
         <h2>Information</h2>
 
-        <table>
-          <tbody>
-            <tr>
-              <th scope="row">Full Name</th>
-              <td>{this.state.fullName}</td>
-            </tr>
-            <tr>
-              <th scope="row">Email</th>
-              <td>{this.state.email}</td>
-            </tr>
-            <tr>
-              <th scope="row">Phone</th>
-              <td>{this.state.phone}</td>
-            </tr>
-          </tbody>
-        </table>
+        {this.state.editMode ? (
+          <div className="container">
+            <div className="field">
+              <label>Full Name</label>
+              <input
+                type="text"
+                name="fullName"
+                value={this.state.fullName}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-        <div className="field">
-          <label>Full Name</label>
-          {this.state.editMode ? (
-            <input
-              type="text"
-              name="fullName"
-              value={this.state.fullName}
-              onChange={this.handleChange}
-              required
-            />
-          ) : (
-            <p>{this.state.fullName}</p>
-          )}
-        </div>
+            <div className="field">
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-        <div className="field">
-          <label>Email</label>
-          {this.state.editMode ? (
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-          ) : (
-            <p>{this.state.email}</p>
-          )}
-        </div>
-
-        <div className="field">
-          <label>Phone</label>
-          {this.state.editMode ? (
-            <input
-              type="tel"
-              name="phone"
-              pattern="0[0-9]{9}"
-              value={this.state.phone}
-              onChange={this.handleChange}
-              required
-            />
-          ) : (
-            <p>{this.state.phone}</p>
-          )}
-        </div>
+            <div className="field">
+              <label>Phone</label>
+              <input
+                type="tel"
+                name="phone"
+                pattern="0[0-9]{9}"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+          </div>
+        ) : (
+          <table>
+            <tbody>
+              <tr>
+                <th scope="row">Full Name</th>
+                <td>{this.state.fullName}</td>
+              </tr>
+              <tr>
+                <th scope="row">Email</th>
+                <td>{this.state.email}</td>
+              </tr>
+              <tr>
+                <th scope="row">Phone</th>
+                <td>{this.state.phone}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
 
         <p className="submitButtonContainer">
           <button>{this.state.editMode ? "Submit" : "Edit"}</button>
