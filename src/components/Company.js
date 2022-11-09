7d3a7a0bf916897df3be59f30a1664a66c1b63ce
Son Do <dominhson580@gmail.com>
2022-11-09 15:03:56 +0700

    Create component for 1 company

---
+++ src/components/Company.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+
+class Company extends Component {
+  constructor() {
+    super();
+    this.state = {
+      company: "",
+      position: "",
+      tasks: "",
+      date: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.editMode ? (
+          <div className="input-group company">
+            <div className="field">
+              <label>Company Name</label>
+              <input
+                type="text"
+                name="company"
+                value={this.state.company}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+
+            <div className="field">
+              <label>Position Title</label>
+              <input
+                type="text"
+                name="position"
+                value={this.state.position}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+
+            <div className="field">
+              <label>Main Tasks</label>
+              <input
+                type="text"
+                name="tasks"
+                value={this.state.tasks}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+
+            <div className="field">
+              <label>Date from and until</label>
+              <input
+                type="text"
+                name="date"
+                value={this.state.date}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+          </div>
+        ) : (
+          <table>
+            <tbody>
+              <tr>
+                <th scope="row">Company Name</th>
+                <td>{this.state.company}</td>
+              </tr>
+              <tr>
+                <th scope="row">Position Title</th>
+                <td>{this.state.position}</td>
+              </tr>
+              <tr>
+                <th scope="row">Main Tasks</th>
+                <td>{this.state.tasks}</td>
+              </tr>
+              <tr>
+                <th scope="row">Date from and until</th>
+                <td>{this.state.date}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Company;
