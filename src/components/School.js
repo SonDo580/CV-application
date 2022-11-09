bf9fefe60e971cdd3f2fb10139e9781b8a85f62f
Son Do <dominhson580@gmail.com>
2022-11-09 14:20:13 +0700

    Use conditional rendering

--- src/components/School.js
+++ src/components/School.js
@@ -18,39 +18,60 @@ class School extends Component {
 
   render() {
     return (
-      <div className="input-group school">
-        <div className="field">
-          <label>School Name</label>
-          <input
-            type="text"
-            name="school"
-            value={this.state.school}
-            onChange={this.handleChange}
-            required
-          />
-        </div>
+      <div>
+        {this.props.editMode ? (
+          <div className="input-group school">
+            <div className="field">
+              <label>School Name</label>
+              <input
+                type="text"
+                name="school"
+                value={this.state.school}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-        <div className="field">
-          <label>Title of Study</label>
-          <input
-            type="text"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-            required
-          />
-        </div>
+            <div className="field">
+              <label>Title of Study</label>
+              <input
+                type="text"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
 
-        <div className="field">
-          <label>Date of study</label>
-          <input
-            type="text"
-            name="date"
-            value={this.state.date}
-            onChange={this.handleChange}
-            required
-          />
-        </div>
+            <div className="field">
+              <label>Date of study</label>
+              <input
+                type="text"
+                name="date"
+                value={this.state.date}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+          </div>
+        ) : (
+          <table>
+            <tbody>
+              <tr>
+                <th scope="row">School Name</th>
+                <td>{this.state.school}</td>
+              </tr>
+              <tr>
+                <th scope="row">Title of Study</th>
+                <td>{this.state.title}</td>
+              </tr>
+              <tr>
+                <th scope="row">Date of Study</th>
+                <td>{this.state.date}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
       </div>
     );
   }
