02410464d7dfcfdd59c2b5201e4b91539e480e3c
Son Do <dominhson580@gmail.com>
2022-11-18 00:34:19 +0700

    Rewrite using function component

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,96 +1,103 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Company extends Component {
-  constructor() {
-    super();
-    this.state = {
-      company: "",
-      position: "",
-      tasks: "",
-      date: "",
-    };
-  }
+const Company = (props) => {
+  const [company, setCompany] = useState("");
+  const [position, setPosition] = useState("");
+  const [tasks, setTasks] = useState("");
+  const [date, setDate] = useState("");
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "company":
+        setCompany(value);
+        break;
+      case "position":
+        setPosition(value);
+        break;
+      case "tasks":
+        setTasks(value);
+        break;
+      case "date":
+        setDate(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  render() {
-    return (
-      <div>
-        {this.props.editMode ? (
-          <div className="input-group company">
-            <div className="field">
-              <label>Company Name</label>
-              <input
-                type="text"
-                name="company"
-                value={this.state.company}
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-
-            <div className="field">
-              <label>Position Title</label>
-              <input
-                type="text"
-                name="position"
-                value={this.state.position}
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-
-            <div className="field">
-              <label>Main Tasks</label>
-              <input
-                type="text"
-                name="tasks"
-                value={this.state.tasks}
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-
-            <div className="field">
-              <label>Date from and until</label>
-              <input
-                type="text"
-                name="date"
-                value={this.state.date}
-                onChange={this.handleChange}
-                required
-              />
-            </div>
+  return (
+    <div>
+      {props.editMode ? (
+        <div className="input-group company">
+          <div className="field">
+            <label>Company Name</label>
+            <input
+              type="text"
+              name="company"
+              value={company}
+              onChange={handleChange}
+              required
+            />
           </div>
-        ) : (
-          <table>
-            <tbody>
-              <tr>
-                <th scope="row">Company Name</th>
-                <td>{this.state.company}</td>
-              </tr>
-              <tr>
-                <th scope="row">Position Title</th>
-                <td>{this.state.position}</td>
-              </tr>
-              <tr>
-                <th scope="row">Main Tasks</th>
-                <td>{this.state.tasks}</td>
-              </tr>
-              <tr>
-                <th scope="row">Date from and until</th>
-                <td>{this.state.date}</td>
-              </tr>
-            </tbody>
-          </table>
-        )}
-      </div>
-    );
-  }
-}
+
+          <div className="field">
+            <label>Position Title</label>
+            <input
+              type="text"
+              name="position"
+              value={position}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="field">
+            <label>Main Tasks</label>
+            <input
+              type="text"
+              name="tasks"
+              value={tasks}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="field">
+            <label>Date from and until</label>
+            <input
+              type="text"
+              name="date"
+              value={date}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+      ) : (
+        <table>
+          <tbody>
+            <tr>
+              <th scope="row">Company Name</th>
+              <td>{company}</td>
+            </tr>
+            <tr>
+              <th scope="row">Position Title</th>
+              <td>{position}</td>
+            </tr>
+            <tr>
+              <th scope="row">Main Tasks</th>
+              <td>{tasks}</td>
+            </tr>
+            <tr>
+              <th scope="row">Date from and until</th>
+              <td>{date}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 export default Company;
