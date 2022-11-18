9e4c37beb76e6291bd63027f627e42b4e390ec99
Son Do <dominhson580@gmail.com>
2022-11-18 08:33:47 +0700

    Rewrite using function component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,41 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import School from "./School";
 
-class Education extends Component {
-  constructor() {
-    super();
-    this.state = {
-      schools: [""],
-    };
-  }
+const Education = (props) => {
+  const [schools, setSchools] = useState([""]);
 
-  addSchool = () => {
-    this.setState((prevState) => ({
-      schools: prevState.schools.concat(""),
-    }));
+  const addSchool = () => {
+    setSchools((prevSchools) => prevSchools.concat(""));
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h2>Education</h2>
+  return (
+    <div className="container">
+      <h2>Education</h2>
 
-        {this.state.schools.map((school, index) => {
-          return <School editMode={this.props.editMode} key={index} />;
-        })}
+      {schools.map((school, index) => {
+        return <School editMode={props.editMode} key={index} />;
+      })}
 
-        {this.props.editMode ? (
-          <p className="buttonContainer">
-            <button type="button" onClick={this.addSchool}>
-              Add School
-            </button>
-          </p>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
-}
+      {props.editMode ? (
+        <p className="buttonContainer">
+          <button type="button" onClick={addSchool}>
+            Add School
+          </button>
+        </p>
+      ) : null}
+    </div>
+  );
+};
 
 export default Education;
